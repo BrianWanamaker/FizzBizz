67c8ba47d33ad76b9d158901cbf79f4cfd4bbae0
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2023-03-21 19:42:13 -0400

    FIZZ

--- Fizz.java
+++ Fizz.java
@@ -1,7 +1,27 @@
+import javax.lang.model.util.ElementScanner6;
+
 public class Fizz{
 
     public static void main(String[]args)
     {
-        
+        for(int i=1;i<101;i++)
+        {
+            if(i%3==0&&i%5==0)
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if(i%3==0)
+            {
+                System.out.println("Fizz");
+            }
+            else if(i%5==0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }    
+        }
     }
 }
